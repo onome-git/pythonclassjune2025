661755af0d5237a567b4bc2deba5119162280004
onome-git <reghaonome@gmail.com>
2025-06-17 09:49:21 -0400

    Update firstpythonclass.py

--- firstpythonclass.py
+++ firstpythonclass.py
@@ -3,3 +3,5 @@ print([1,2,3,4,5,6,7,8,9,10])
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Sum is:", a + b)
+import random
+print("Random number:", random.randint(1, 100))
